faf06372db7da33be8bcfcfa0d257931bceaeee6
sunbvert <sunchuanop@outlook.com>
2018-10-11 16:11:02 +0800

    可以作图啦

--- app/src/main/java/com/sunbvert/lifeexpectancy/DisplayData.java
+++ app/src/main/java/com/sunbvert/lifeexpectancy/DisplayData.java
@@ -1,11 +1,18 @@
 package com.sunbvert.lifeexpectancy;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.JsonReader;
 import android.util.Log;
+import android.view.animation.Animation;
 
+import com.github.mikephil.charting.animation.ChartAnimator;
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
@@ -21,6 +28,7 @@ import java.util.List;
 public class DisplayData extends AppCompatActivity {
 
     static String TAG = "数据展示界面";
+    static String TAG2 = "线程：读取JOSN数据" ;
     static String fileName = "life-expectancy.json";
 
     LineChart chart;
@@ -50,7 +58,7 @@ public class DisplayData extends AppCompatActivity {
 
 
 
-    private void drawLineChart(LifeExpValuesForPlotting valuesForPlotting){
+    public void drawLineChart(LifeExpValuesForPlotting valuesForPlotting){
         List<Entry> popuEntries = new ArrayList<Entry>();
         List<Entry> ageEntries = new ArrayList<Entry>();
         List<Entry> gdpEntries = new ArrayList<Entry>();
@@ -58,23 +66,28 @@ public class DisplayData extends AppCompatActivity {
         //int entrySize = valuesForPlotting.averageLifeTime.length;
         int dataSize = valuesForPlotting.gdp.length;
 
-        String selectedCountry = valuesForPlotting.counties[0];
+        String selectedCountry = valuesForPlotting.counties[2];
 
-        float j = 0;
         for(int i = 0; i < dataSize; i++){
             if (valuesForPlotting.counties[i].equals(selectedCountry)){
-                popuEntries.add(new Entry(j, valuesForPlotting.population[i]));
-                ageEntries.add(new Entry(j, (float)valuesForPlotting.averageLifeTime[i]));
-                gdpEntries.add(new Entry(j, valuesForPlotting.gdp[i]));
+                popuEntries.add(new Entry(valuesForPlotting.investigateYear[i],
+                        valuesForPlotting.population[i] / 10000000));
+                ageEntries.add(new Entry(valuesForPlotting.investigateYear[i],
+                        (float)valuesForPlotting.averageLifeTime[i]));
+                gdpEntries.add(new Entry(valuesForPlotting.investigateYear[i],
+                        valuesForPlotting.gdp[i] / 100));
             }
         }
 
         LineDataSet setPopu = new LineDataSet(popuEntries, "人口数");
         setPopu.setAxisDependency(YAxis.AxisDependency.LEFT);
+        setPopu.setColor(Color.BLUE);
         LineDataSet setAge = new LineDataSet(ageEntries, "平均年龄");
         setAge.setAxisDependency(YAxis.AxisDependency.LEFT);
+        setAge.setColor(Color.GREEN);
         LineDataSet setGdp = new LineDataSet(gdpEntries, "GDP");
         setGdp.setAxisDependency(YAxis.AxisDependency.LEFT);
+        setGdp.setColor(Color.RED);
 
         List<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
         dataSets.add(setPopu);
@@ -83,7 +96,155 @@ public class DisplayData extends AppCompatActivity {
 
         LineData data = new LineData(dataSets);
 
+        Description description = new Description();
+        description.setText(selectedCountry);
+
         chart.setData(data);
+        chart.setDescription(description);
+        chart.animateXY(3000, 3000);
         chart.invalidate();
+    }
+
+
+
+    private class LoadJsonData extends AsyncTask<JsonReader, Integer, LifeExpectancyValues> {
+
+        @Override
+        protected LifeExpectancyValues doInBackground(JsonReader[] reader) {
+            LifeExpectancyValues values = new LifeExpectancyValues();
+            try{
+                values = readObjects(reader[0]);
+            } catch (IOException e){
+                Log.e(TAG2, "错误信息：", e);
+            }
+            return values;
+        }
+
+        @Override
+        protected void onPostExecute(LifeExpectancyValues result) {
+            //test code
+            SplitJsonData asyncSplitData = new SplitJsonData();
+            asyncSplitData.execute(result);
+        }
+
+        private LifeExpectancyValues readObjects(JsonReader reader) throws IOException {
+            List<String> counties = null;
+            List<Integer> timeLine = null;
+            List<List<String>> lifeExpectancyValues = null;
+
+            reader.beginObject();
+            while(reader.hasNext()){
+                String name = reader.nextName();
+                switch (name) {
+                    case "counties":
+                        counties = readStringArray(reader);
+                        break;
+                    case "timeline":
+                        timeLine = readIntegerArray(reader);
+                        break;
+                    case "series":
+                        lifeExpectancyValues = readThreeDimArray(reader);
+                        break;
+                }
+            }
+            reader.endObject();
+
+            return new LifeExpectancyValues(counties, timeLine, lifeExpectancyValues);
+        }
+
+        private List<String> readStringArray(JsonReader reader) throws IOException {
+            List<String> strings = new ArrayList<String>();
+
+            reader.beginArray();
+            while(reader.hasNext()){
+                strings.add(reader.nextString());
+            }
+            reader.endArray();
+            return strings;
+        }
+
+        private List<Integer> readIntegerArray(JsonReader reader) throws IOException {
+            List<Integer> integers = new ArrayList<Integer>();
+
+            reader.beginArray();
+            while(reader.hasNext()){
+                integers.add(reader.nextInt());
+            }
+            reader.endArray();
+            return integers;
+        }
+
+        private List<List<String>> readThreeDimArray(JsonReader reader) throws IOException {
+            List<List<String>> lifeExpectancyValues = new ArrayList<List<String>>();
+
+            reader.beginArray();
+            while(reader.hasNext()){
+                reader.beginArray();
+                while(reader.hasNext()){
+                    List<String> values = new ArrayList<String>();
+                    reader.beginArray();
+                    while(reader.hasNext()){
+                        values.add(reader.nextString());
+                    }
+                    reader.endArray();
+                    lifeExpectancyValues.add(values);
+                }
+                reader.endArray();
+            }
+            reader.endArray();
+
+            return lifeExpectancyValues;
+        }
+    }
+
+
+    private class SplitJsonData extends AsyncTask<LifeExpectancyValues, Integer, LifeExpValuesForPlotting> {
+
+        @Override
+        protected LifeExpValuesForPlotting doInBackground(LifeExpectancyValues... lifeExpectancyValues) {
+            return splitValues(lifeExpectancyValues[0]);
+        }
+
+        @Override
+        protected void onPostExecute(LifeExpValuesForPlotting result){
+            drawLineChart(result);
+        }
+
+        private LifeExpValuesForPlotting splitValues(LifeExpectancyValues values){
+            int dataSize = values.lifeExpectancyValues.size();
+            //int valueSize = LifeExpValuesForPlotting.valueSize;  //valueSize = 5
+
+            int[] gdp = new int[dataSize];
+            double[] averageLifeTime = new double[dataSize];
+            long[] population = new long[dataSize];
+            String[] counties = new String[dataSize];
+            int[] investigateYear = new int[dataSize];
+
+            int j = 0;
+            for (List<String> valueList: values.lifeExpectancyValues) {
+                for (int i = 0; i < valueList.size(); i++) {
+                    switch (i){
+                        case 0:
+                            gdp[j] = Integer.parseInt(valueList.get(i));
+                            break;
+                        case 1:
+                            averageLifeTime[j] = Double.parseDouble(valueList.get(i));
+                            break;
+                        case 2:
+                            population[j] = Long.parseLong(valueList.get(i));
+                            break;
+                        case 3:
+                            counties[j] = valueList.get(i);
+                            break;
+                        case 4:
+                            investigateYear[j] = Integer.parseInt(valueList.get(i));
+                            break;
+                    }
+                }
+                j++;
+            }
+
+            return new LifeExpValuesForPlotting(gdp, averageLifeTime, population, counties, investigateYear);
+        }
     }
 }
